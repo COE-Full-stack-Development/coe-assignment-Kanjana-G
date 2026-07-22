ac0386ae9b3cdc5f19f49b6dd3442ccacc1c0246
Kanjana-G <govindarajkanjana@gmail.com>
2026-07-22 15:28:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 (() => {
-  
-// your code goe here
 
-  });
+let themeBtn = document.getElementById("toggle-theme-btn");
+let card = document.getElementById("profile-card");
+
+themeBtn.addEventListener("click", function () {
+    card.classList.toggle("dark-theme");
+});
+
+let addBtn = document.getElementById("add-skill-btn");
+let input = document.getElementById("skill-input");
+let list = document.getElementById("skills-list");
+
+addBtn.addEventListener("click", function () {
+
+    if (input.value.trim() != "") {
+
+        let li = document.createElement("li");
+        li.innerText = input.value;
+
+        list.appendChild(li);
+
+        input.value = "";
+    }
+
+});
+
 })();
